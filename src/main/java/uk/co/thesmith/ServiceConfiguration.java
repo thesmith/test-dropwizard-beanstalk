d2045a9c844188f051950289387832c1ff8d9c45
Ben Smith <ben@thesmith.co.uk>
2015-06-17 13:51:57 +0100

    first commit

---
+++ src/main/java/uk/co/thesmith/ServiceConfiguration.java
@@ -0,0 +1,6 @@
+package uk.co.thesmith;
+
+import io.dropwizard.Configuration;
+
+public class ServiceConfiguration extends Configuration {
+}
---
+++ src/main/java/uk/co/thesmith/resources/HealthCheckResource.java
@@ -0,0 +1,13 @@
+package uk.co.thesmith.resources;
+
+import javax.ws.rs.*;
+
+@Path("/health")
+@Produces("application/json; charset=utf-8")
+public class HealthCheckResource extends BaseResource {
+  @GET
+  @Path("/check")
+  public String doCheck() throws Exception {
+    return "ok";
+  }
+}
